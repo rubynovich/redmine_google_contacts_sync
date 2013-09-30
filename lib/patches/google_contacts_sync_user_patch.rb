9e6f6ee94609ec85a7e5d56462dfa6eadc1bf68d
max <max@koritskiy.ru>
2013-09-30 16:35:58 +0400

    fix sync now

--- lib/patches/google_contacts_sync_user_patch.rb
+++ lib/patches/google_contacts_sync_user_patch.rb
@@ -16,9 +16,9 @@ module GoogleContactsSyncPlugin
       end
 
       def run_google_sync
-        if (User.no_set_google_sync.nil? || (! User.no_set_google_sync.include?(self.id))) && Setting[:plugin_redmine_google_contacts_sync] && (Setting[:plugin_redmine_google_contacts_sync][:sync_after_save].to_i == 1)
-          UserGoogleContact.sync_all!
-        end
+        #if (User.no_set_google_sync.nil? || (! User.no_set_google_sync.include?(self.id))) && Setting[:plugin_redmine_google_contacts_sync] && (Setting[:plugin_redmine_google_contacts_sync][:sync_after_save].to_i == 1)
+        #  UserGoogleContact.sync_all!
+        #end
       end
 
   end
