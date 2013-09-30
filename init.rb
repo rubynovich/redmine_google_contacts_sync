bdd0590c06c6712564cc85052ac6a61afeaf4327
max <max@koritskiy.ru>
2013-09-30 11:13:30 +0400

    add: apply patches to init.rb for plugin

--- init.rb
+++ init.rb
@@ -8,4 +8,19 @@ Redmine::Plugin.register :redmine_google_contacts_sync do
   settings :partial => 'google_contacts_sync/settings'
 end
 
+Rails.configuration.to_prepare do
+
+  [:user].each do |cl|
+    require "patches/google_contacts_sync_#{cl}_patch"
+  end
+
+  require_dependency 'user'
+
+  [
+      [User, GoogleContactsSyncPlugin::UserPatch],
+  ].each do |cl, patch|
+    cl.send(:include, patch) unless cl.included_modules.include? patch
+  end
+
+end
 
